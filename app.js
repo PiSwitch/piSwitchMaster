f9436cd897ef29e4b2844c6644691d79f1a12986
Christophe Gagnier <admin@cgagnier.ca>
2017-03-25 22:38:47 -0400

    Use pug as a rendering engine

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
 var config = require('./config.json');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var path = require('path');
+var logger = require('morgan');
+var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
 var mysql = require('mysql');
 var pool = mysql.createPool(config.mysql);
 
-var webRoot = __dirname + '/www/';
+var app = express();
+var server = http.Server(app);
+io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
 
 app.get('/', function(req, res){
-    res.sendFile(webRoot + 'index.html');
+    res.render('index')
 });
 app.get('/test_sql', function(req, res){
     pool.query('SELECT * FROM `test` ', function (error, results, fields) {
         if (error) throw error;
         res.send(results);
-        console.log('The solution is: ', results[0].solution);
+    });
+});
+
+
+// 404
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Server errors
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
     });
 });
 
@@ -26,7 +52,7 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(config.port, config.hostname, function(){
+server.listen(config.port, config.hostname, function(){
     var hostname = config.hostname ? config.hostname : '*';
-    console.log('listening on ' + hostname + ':' + http.address().port);
+    console.log('listening on ' + hostname + ':' + server.address().port);
 });
